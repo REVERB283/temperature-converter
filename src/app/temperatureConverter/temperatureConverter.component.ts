a9dc87cd091c6a4387f223b3a0cffaa01e3f2801
Harshit Anand <harshitanand283@gmail.com>
2022-10-29 17:52:29 +0530

    initial commit

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -1,16 +1,33 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'temperature-converter',
-  templateUrl: './temperatureConverter.component.html',
-  styleUrls: ['./temperatureConverter.component.scss']
+	selector: "temperature-converter",
+	templateUrl: "./temperatureConverter.component.html",
+	styleUrls: ["./temperatureConverter.component.scss"],
 })
-
 export class TemperatureConverter implements OnInit {
+	celcius: string;
+	fahrenheit: string;
 
-  ngOnInit() {
-    // C = (F − 32) × 5/9
-    // F = C*9/5 + 32
-  }
+	constructor() {
+		this.celcius = "";
+		this.fahrenheit = "";
+	}
 
+	ngOnInit() {
+		// C = (F − 32) × 5/9
+		// F = C*9/5 + 32
+	}
+
+	changeTemparutue(value, type) {
+		switch (type) {
+			case "c":
+				this.fahrenheit = `${((Number(value) * 9) / 5 + 32).toFixed(1)}`;
+				break;
+
+			case "f":
+				this.celcius = `${(((Number(value) - 32) * 5) / 9).toFixed(1)}`;
+				break;
+		}
+	}
 }
